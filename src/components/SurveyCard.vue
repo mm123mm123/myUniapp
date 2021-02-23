38adfd72cc4fba0d0814ba3564162cae7323ef33
杨泽 <1083849984@qq.com>
2021-02-23 10:14:06 +0800

    勘察卡片

---
+++ src/components/SurveyCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <view class="card-wrapper">
+    <view class="head">
+      <view class="enterpriseName">{{ enterpriseName }}</view>
+      <view class="status">{{ status }}</view>
+    </view>
+    <view class="body">
+      <span class="category">{{ category }}</span>
+    </view>
+    <view class="footer">
+      <button>历史</button>
+      <button>查看</button>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  props: {
+    enterpriseName: '',
+    status: '',
+    category: ''
+  }
+}
+</script>
+<style lang="scss" scoped>
+.card-wrapper {
+  font-size: 16px;
+  border: 1px solid red;
+  padding: 15px 15px 10px 15px;
+
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .body {
+    border-bottom: 2px solid #f8f8f8;
+    padding: 12px 0;
+
+    .category {
+      padding: 0 8px;
+      border-radius: 18px;
+      background-color: #f8f8f8;
+    }
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: 10px;
+
+    button {
+      margin-left: 15px;
+      padding: 4px 14px;
+      border-radius: 56px;
+      background-color: #ffffff;
+      border: 1px solid #cccccc;
+      color: #666666;
+      font-size: 12px;
+    }
+  }
+}
+
+</style>
--- src/pages/calendar/index.vue
+++ src/pages/calendar/index.vue
@@ -1,15 +1,57 @@
 <template>
   <view>
-    <Calendar></Calendar>
-
+    <view class="calendar">
+      <Calendar></Calendar>
+    </view>
+    <view class="content-wrapper">
+      <view class="slide-button">
+        <image :src="picPath" class="downArrow"></image>
+        <span>
+          {{ btnText }}
+        </span>
+      </view>
+      <view class="content">
+        <SurveyCard v-for="card in cardList"
+                    :enterpriseName="card.enterpriseName"
+                    :status="card.status" :category="card.category">
+        </SurveyCard>
+      </view>
+    </view>
   </view>
 </template>
 <script>
 import Calendar from '@dcloudio/uni-ui/lib/uni-calendar/uni-calendar.vue'
+import SurveyCard from "@/components/SurveyCard";
 
 export default {
   components: {
-    Calendar
+    Calendar,
+    SurveyCard
+  },
+  data() {
+    return {
+      btnText: '展开',
+      picPath: '../../static/down@2x.png',
+      cardList: [
+        {enterpriseName: '黑狼口', status: '已驳回', category: '医疗卫生机构'},
+        {enterpriseName: '王麻子村', status: '已驳回', category: '村庄'}
+      ]
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.content-wrapper {
+  border: 1px solid blue;
+
+  .slide-button {
+    border: 1px solid red;
+    text-align: center;
+
+    .downArrow {
+      width: 15px;
+      height: 15px;
+    }
+  }
+}
+</style>
